560bb884bcb0a93d5307f2bb466f4504579fc321
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-12-11 23:03:42 +0300

    showing/hiding cart section added

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCardHandler = () => {
+    dispatch(uiActions.toogle())
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCardHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
